e383441d01b07e4d6550872c832a03c90cf69da8
lan496 <kohei19950508@gmail.com>
2017-12-13 18:41:42 +0900

    Wang-Landau

---
+++ include/MCMC.hpp
@@ -0,0 +1,8 @@
+#ifndef MCMC_H
+#define MCMC_H
+
+#include <vector>
+
+void Wang_Landau(int, int, std::vector<double>);
+
+#endif
---
+++ lib/MCMC.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+
+#include "../include/system.hpp"
+
+bool is_flat(const std::vector<int>& hist) {
+  int N = hist.size();
+
+  bool ret = true;
+  int cnt = 0;
+  for(int i = 0; i < N - 1; ++i) {
+    if(!hist[i] || !hist[i + 1]) continue;
+    if((double)std::abs(hist[i] - hist[i + 1]) / hist[i] > 0.1) ret = false;
+    ++cnt;
+  }
+
+  if(cnt < N / 3) ret = false;
+  return ret;
+}
+
+void Wang_Landau(int L, int delta, std::vector<double> weights) {
+  int V = (L + 1) * (L + 1);
+
+  int Emax = 3 * V;
+  int Emin = 0;
+
+  int N = (Emax - Emin + delta - 1) / delta;
+  std::vector<int> hist(N, 0);
+  std::vector<double> entropy(N, 0);
+  double f = 1.0;
+
+  double eps = 1e-2;
+
+  std::random_device rnd;
+  std::mt19937 mt(rnd());
+  std::uniform_real_distribution<double> bern(0.0, 1.0);
+
+  Configuration current(V);
+  for(int i = 0; i < V; ++i) {
+    current[i] = mt() % 2;
+  }
+  int current_e = energy(current, L);
+
+  while(f > eps) {
+    int propose_idx = mt() % V;
+    int proposed_e = proposed_energy(current, L, propose_idx);
+
+    if(bern(mt) < std::exp(entropy[current_e] - entropy[proposed_e])) {
+      current_e = proposed_e;
+      current[propose_idx] = !current[propose_idx];
+    }
+
+    hist[current_e / delta] += 1;
+    entropy[current_e / delta] += f;
+
+    for(int i = 0; i < V; ++i) std::cerr << " " << entropy[i];
+    std::cerr << std::endl;
+
+    if(is_flat(hist)) {
+      for(int i = 0; i < N; ++i) {
+        hist[i] = 0;
+      }
+      f *= 0.5;
+    }
+  }
+
+  double entropy_tot = 0;
+  for(int i = 0; i < N; ++i) entropy_tot += entropy[i];
+
+  weights.assign(N, 0);
+  for(int i = 0; i < N; ++i) weights[i] = std::exp(entropy[i] / entropy_tot);
+
+  for(int i = 0; i < N; ++i) std::cerr << " " << weights[i];
+  std::cerr << std::endl;
+}
